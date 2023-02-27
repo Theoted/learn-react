cb80843ad7349b19441fa1bd5af577e67d5c0023
Théo <74186192+Theoted@users.noreply.github.com>
2023-02-27 21:38:59 +0100

    Form management, next step -> multiple component

--- src/App.js
+++ src/App.js
@@ -22,18 +22,39 @@ import './App.css';
 // }
 
 // List management
+// Form Management
 
 function App() {
   const [fruits, setFruits] = useState([
-    {id: 1, name: "Banane"},
-    {id: 2, name: "Pomme"},
-    {id: 3, name: "Cerise"}
+    { id: 1, name: "Banane" },
+    { id: 2, name: "Pomme" },
+    { id: 3, name: "Cerise" }
   ]);
 
-  const removeFruit = (id) => {
+  const [newFruit, setNewFruit] = useState("");
+
+  const handleClick = (id) => {
     const fruitsCopy = [...fruits];
     const fruitsCopyUpdated = fruitsCopy.filter((fruit) => fruit.id !== id);
     setFruits(fruitsCopyUpdated);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const fruitsCopy = [...fruits];
+
+    const id = new Date().getTime();
+    const name = newFruit;
+
+    fruitsCopy.push({ id, name });
+
+    setFruits(fruitsCopy);
+    setNewFruit("");
+  }
+
+  const handleChange = (event) => {
+    setNewFruit(event.target.value);
   }
 
   return (
@@ -42,10 +63,15 @@ function App() {
       <ul>
         {fruits.map((fruit) => (
           <li key={fruit.id}>
-            {fruit.name} <button onClick={() => removeFruit(fruit.id)}>X</button>
+            {fruit.name} <button onClick={() => handleClick(fruit.id)}>X</button>
           </li>
         ))}
       </ul>
+      <h2>Add Fruit</h2>
+      <form action="post" onSubmit={handleSubmit}>
+        <input type="text" value={newFruit} onChange={handleChange} />
+        <button type="submit">Submit</button>
+      </form>
     </>
   )
 }
