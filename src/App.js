d0b2017f05d49d4cc57c7c714b1d137dd5b95c4f
Théo <74186192+Theoted@users.noreply.github.com>
2023-02-27 20:58:18 +0100

    List management using map, useState

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
 import { useState } from 'react';
 import './App.css';
 
-function  App() {
-   // State
-    const [value, setValue] = useState(1);
+// My first component
 
-   // Event
-   const increment = () => {
-    setValue(value + 1);
-   }
+// function  App() {
+//    // State
+//     const [value, setValue] = useState(1);
 
-   // Render
-   return (
+//    // Event
+//    const increment = () => {
+//     setValue(value + 1);
+//    }
+
+//    // Render
+//    return (
+//     <>
+//       <h1>{value}</h1>
+//       <button onClick={increment}>Increment</button>
+//     </>
+//    )
+// }
+
+// List management
+
+function App() {
+  const [fruits, setFruits] = useState([
+    {id: 1, name: "Banane"},
+    {id: 2, name: "Pomme"},
+    {id: 3, name: "Cerise"}
+  ]);
+
+  const removeFruit = (id) => {
+    const fruitsCopy = [...fruits];
+    const fruitsCopyUpdated = fruitsCopy.filter((fruit) => fruit.id !== id);
+    setFruits(fruitsCopyUpdated);
+  }
+
+  return (
     <>
-      <h1>{value}</h1>
-      <button onClick={increment}>Increment</button>
+      <h1>Fruit list</h1>
+      <ul>
+        {fruits.map((fruit) => (
+          <li key={fruit.id}>
+            {fruit.name} <button onClick={() => removeFruit(fruit.id)}>X</button>
+          </li>
+        ))}
+      </ul>
     </>
-   )
+  )
 }
 
 export default App;
